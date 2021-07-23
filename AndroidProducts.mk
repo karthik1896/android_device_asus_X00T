5928df8b68aef0b557880cbe6fc9bfba76954147
karthik1896 <karthik1896@outlook.com>
2021-07-23 06:04:13 +0000

    Revert "X00T: Bringup Havoc"
    This reverts commit 890f3d8d946354c44b52fc122492833dba01a0b9.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_X00T.mk
+    $(LOCAL_DIR)/lineage_X00T.mk
 
 COMMON_LUNCH_CHOICES := \
-      havoc_X00T-userdebug
+      lineage_X00T-userdebug
