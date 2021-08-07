b617477912b3c71b5282ba7f18cc6ef0fc1903d2
sbwml <984419930@qq.com>
2021-08-07 10:08:09 +0530

    X00T: Update portion IMS blobs from  LA.UM.9.6.2.r1-02500-89xx.0
    - com.qualcomm.qti.uceservice 2.2
    - vendor.qti.imsrtpservice 3.0
    - camera.provider 2.6

--- device.mk
+++ device.mk
@@ -146,10 +146,9 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@3.4 \
+	android.hardware.camera.provider@2.6:64 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
     libxml2 \
     libgui_vendor \
