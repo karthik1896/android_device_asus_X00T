8c02d237fd0de9d65c8e96111bc08cf64946c1b7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-20 16:59:50 +0530

    X00T:Drop product_launched_with_*.mk
    * It seems Google stopped making those
    * Directly set PRODUCT_SHIPPING_API_LEVEL
    
    Change-Id: If00badd6dc12e0245c74bba12e4816361091e754
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- voltage_X00T.mk
+++ voltage_X00T.mk
@@ -20,7 +20,6 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Voltage stuff
 $(call inherit-product, vendor/voltage/config/common_full_phone.mk)
