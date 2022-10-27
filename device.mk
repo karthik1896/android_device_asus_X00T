e3f0b1eb52e22da29844d6d031ef70d6493006a7
karthik1896 <karthik1896@outlook.com>
2022-10-27 10:28:47 +0530

    X00T: Drop cryptfs_hw
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- device.mk
+++ device.mk
@@ -231,10 +231,6 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
