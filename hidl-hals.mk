4197bb19ec713b48c1259b371a255e8a504a0875
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-03-17 11:34:41 +0530

    X00T: Drop QTI camera device usage
    * qcom-camera topic hasn't been ported to 19.1
    * Keep building vendor.qti.hardware.camera.device@1.0 interface lib, IMS stack and possibly camera HAL still needs it
    
    Change-Id: I87bcd5b54ad986d5844df50de243fc1a18507198

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -47,7 +47,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Configstore
 PRODUCT_PACKAGES += \
