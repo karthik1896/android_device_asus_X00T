Project404-android-asus-sdm660/device_asus_X00T

X00T: Disable ART debug and optimize dexpreopt
 * minimize java debug info
 * use everything filter for dexpreopt (https://github.com/AOSPA/android_vendor_pa/commit/ab5061c425e61bc7d36f7bcf480feea975795545)
 * disable debug configs in art
 * use non-debug dex2oat

Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

device.mk
@@ -119,6 +119,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# Dex/ART optimization
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+USE_DEX2OAT_DEBUG := false
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \

karthik1896/android_device_asus_X00T

device.mk
@@ -86,6 +86,12 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Dex/ART optimization
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+USE_DEX2OAT_DEBUG := false
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \